1b1e2a91a5ac79c126fb42cd27f7245bddfdbd56
Waldo Mauricio BC <waldomauriciobc@gmail.com>
2024-10-09 08:43:00 -0500

    w03

--- database/index.js
+++ database/index.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg")
 require("dotenv").config()
-/* ***********************
+/* **************
  * Connection Pool
- * SSL Object needed for local test of app
- * But will cause problems in production enviroment
- * IF - else will make a determination which to use
- * ********************* */
-let pool // changed to lower case pool becasue vscode was showing error in the code.
+ * SSL Object needed for local testing of app
+ * But will cause problems in production environment
+ * If - else will make determination which to use
+ * ************** */
+let pool
 if (process.env.NODE_ENV == "development") {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
@@ -15,20 +15,20 @@ if (process.env.NODE_ENV == "development") {
         },
     })
 
-// Added for troubleshooting queries
-// during development
-module.exports = {
-    async query(text, params) {
-        try {
-            const res = await pool.query(text, params)
-            console.log("executed query", { text })
-            return res
-        } catch (error) {
-            console.error("error in quer", { test })
-            throw error
-        }
-    },
-}
+    // Added for troubleshooting queries
+    // during development
+    module.exports = {
+        async query(text, params) {
+            try {
+                const res = await pool.query(text, params)
+                console.log("executed query", { text })
+                return res
+            } catch (error) {
+                console.error("error in query", { text })
+                throw error
+            }
+        },
+    }
 } else {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
