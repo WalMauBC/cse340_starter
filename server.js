4a98f14ca777e39b40f62e1f4ad44d8bc02b1d0e
Waldo Mauricio BC <waldomauriciobc@gmail.com>
2024-10-21 13:48:32 -0500

    W05
    Regrese los cambios porque no se publicaba la pagina

--- server.js
+++ server.js
@@ -12,11 +12,10 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require('./utilities/')
-const session = require("express-session")
-const pool = require('./database')
 const accountRoute = require("./routes/accountRoute")
-const bcrypt = require("bcrypt")
+const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require('./database/')
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
 
@@ -36,17 +35,17 @@ app.use(session({
 
 // Express Messages Middleware
 app.use(require('connect-flash')())
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
-
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use(cookieParser())
 app.use(utilities.checkJWTToken)
+
 
 /* ***********************
  * Routes
@@ -65,10 +64,7 @@ app.get("/", baseController.buildHome)
 app.use("/inv", inventoryRoute)
 // Account route
 app.use("/account", accountRoute)
-//Cookie Parser
-app.use(cookieParser())
 
-app.use(utilities.checkJWTToken)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
