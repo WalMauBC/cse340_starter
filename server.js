e3ada795d0c1100d4a5598c90f3dc18b022bb8a1
Waldo Mauricio BC <waldomauriciobc@gmail.com>
2024-10-22 11:52:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities/")
 const session = require("express-session")
 const pool = require('./database/')
-const bodyParser = require("body-parser")
+
 const cookieParser = require("cookie-parser")
 
 /********** 
@@ -40,11 +40,7 @@ app.use(function (req, res, next) {
   next()
 })
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.use(cookieParser())
-app.use(utilities.checkJWTToken)
 
 
 /* ***********************
