8ff6ddf6b1c9d3186aaf1227c2fd064e5c3aeaa4
Waldo Mauricio BC <waldomauriciobc@gmail.com>
2024-10-30 18:58:44 -0500

    Update inventory-model.js
    Se actualiza para que se vean los carros a detalles

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,12 +1,10 @@
 const pool = require("../database/")
-
 /* ***************************
  *  Get all classification data
  * ************************** */
 async function getClassifications(){
   return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
-
 /* ***************************
  *  Get all inventory items and classification_name by classification_id
  * ************************** */
@@ -24,7 +22,6 @@ async function getInventoryByClassificationId(classification_id) {
     console.error("getClassificationsById error " + error)
   }
 }
-
 async function getInventoryByInvId(inv_id) {
   try {
     const data = await pool.query(
@@ -32,12 +29,11 @@ async function getInventoryByInvId(inv_id) {
        WHERE i.inv_id = $1`,
        [inv_id]
     )
-    return data.rows
+    return data.rows[0]
   } catch (error) {
     console.error("getInventoryByInvId error " + error)
   }
 }
-
 async function addClassification(classification_name) {
   try {
     const sql = "INSERT INTO classification (classification_name) VALUES ($1)"
@@ -47,7 +43,6 @@ async function addClassification(classification_name) {
     console.error("Failed to add classification name: ", error)
   }
 }
-
 async function checkExistingName(classification_name) {
   try {
     const sql = "SELECT * FROM classification WHERE classification_name = $1"
@@ -58,7 +53,6 @@ async function checkExistingName(classification_name) {
     return 'This classification name already exists'
   }
 }
-
 async function addInventory(
   inv_make,
   inv_model,
@@ -80,5 +74,13 @@ async function addInventory(
     console.error("Adding vehicle data failed")
   }
 }
-
-module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByInvId, addClassification, checkExistingName, addInventory}
+async function deleteInventoryItem (inv_id) {
+  try {
+    const sql = "DELETE FROM inventory WHERE inv_id = $1"
+    const data = await pool.query(sql, [inv_id])
+    return data
+  } catch (error) {
+    new Error("Delete Inventory Error")
+  }
+}
+module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByInvId, addClassification, checkExistingName, addInventory, deleteInventoryItem}
